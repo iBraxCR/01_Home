e85815f19051f5feb43d4d002bc0553fd9a8ce08
iBrax <joschuabarahona15@gmail.com>
2023-10-14 21:11:30 -0600

    Esta fue mi primera clase

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        final int entero = 12345; //Numeros si quiero colocar que la variable no cambie se le coloca un "final" si quiero que sea variable simplemente se le quita el final
+        float decimal = 15.5f; //Cuando es float siempre debe de tener una "F" al final
+        char caracter = 'a'; //Variable solo de un caracter 
+        boolean desicion = true; //Colocar si es true o false
+        String text = "Hola mundo";
+
+        
+    }
+}
